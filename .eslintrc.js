4eb848616f52eeacc58335cd643faf617b04f98d
Viet Ngoc <ngocjohn@me.com>
2024-09-21 05:35:57 +0200

    chore: Update eslint configuration and package.json scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:perfectionist/recommended-natural-legacy'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
