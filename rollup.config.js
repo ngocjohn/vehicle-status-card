d43d53ddd9f990cf13c841187eb71f9b80d754d7
Viet Ngoc <ngocjohn@me.com>
2025-02-16 22:11:41 +0100

    chore: Update dependencies and refactor Rollup configuration to use new plugin imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
@@ -41,7 +41,7 @@ const replaceOpts = {
 };
 
 const plugins = [
-  nodeResolve({}),
+  nodeResolve(),
   commonjs(),
   typescript(),
   json(),
