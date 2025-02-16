02915e7a18c17b62a14e2abb65540e35683440c7
Viet Ngoc <ngocjohn@me.com>
2025-02-16 21:57:24 +0100

    chore: Update dependencies and improve Rollup configuration

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
@@ -41,7 +42,7 @@ const replaceOpts = {
 };
 
 const plugins = [
-  nodeResolve(),
+  nodeResolve({ preferBuiltins: false }),
   commonjs(),
   typescript(),
   json(),
