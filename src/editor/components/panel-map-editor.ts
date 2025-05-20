48749217516f7b58bb959221dd6153821351b37c
Viet Ngoc <ngocjohn@me.com>
2025-05-20 20:36:23 +0200

    refactor: improve map editor functionality and YAML handling

--- src/editor/components/panel-map-editor.ts
+++ src/editor/components/panel-map-editor.ts
@@ -2,15 +2,15 @@ import { EntityConfig } from 'custom-card-helpers';
 import { processConfigEntities } from 'extra-map-card';
 import { ExtraMapCardConfig, MapEntityConfig } from 'extra-map-card/dist/types/config';
 import { LitElement, html, TemplateResult, CSSResultGroup, PropertyValues } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
+import { customElement, property, query, state } from 'lit/decorators.js';
 
 import editorcss from '../../css/editor.css';
 import { VehicleStatusCardConfig, HomeAssistant, fireEvent, MiniMapConfig, LovelaceConfig } from '../../types';
 import { Create } from '../../utils';
-import { _convertToExtraMapConfig, hasLocation } from '../../utils/ha-helper';
+import { _convertToExtraMapConfig } from '../../utils/ha-helper';
 import { VehicleStatusCardEditor } from '../editor';
 import { ALERT_INFO } from '../editor-const';
-import { singleMapConfingSchema, baseMapConfigSchema, miniMapConfigSchema } from '../form';
+import { baseMapConfigSchema, miniMapConfigSchema } from '../form';
 
 @customElement('panel-map-editor')
 export class PanelMapEditor extends LitElement {
@@ -20,20 +20,31 @@ export class PanelMapEditor extends LitElement {
   @property({ attribute: false }) _config!: VehicleStatusCardConfig;
   @state() _mapCardConfig?: MiniMapConfig;
   @state() _mapEntitiesConfig?: EntityConfig[];
-  @state() _yamlEditor: boolean = false;
+  @state() _yamlMode: boolean = false;
   @state() _useSingleMapCard: boolean = false;
 
-  private get _mapConfig(): MiniMapConfig {
-    return this._config?.mini_map || {};
-  }
+  @state() private _tmpYamlConfig?: ExtraMapCardConfig;
+  @query('hui-card-element-editor') private _mapCardEditor?: HTMLElement;
 
   static get styles(): CSSResultGroup {
     return [editorcss];
   }
 
+  private get _mapConfig(): MiniMapConfig {
+    return this._config?.mini_map || {};
+  }
+
   private get _extraMapCardConfig(): ExtraMapCardConfig {
     const mapConfig = _convertToExtraMapConfig(this._mapConfig, this._mapEntitiesConfig);
     return mapConfig;
+  }
+
+  private get _deviceTrackerEntity(): MapEntityConfig {
+    return {
+      entity: this._mapConfig.device_tracker,
+      label_mode: this._mapConfig.label_mode,
+      attribute: this._mapConfig.attribute,
+    };
   }
 
   protected updated(_changedProperties: PropertyValues): void {
@@ -43,8 +54,8 @@ export class PanelMapEditor extends LitElement {
         ...(this._config.mini_map || {}),
       };
       this._mapEntitiesConfig = this._mapCardConfig.extra_entities
-        ? processConfigEntities<MapEntityConfig>(this._mapCardConfig.extra_entities)
-        : [];
+        ? processConfigEntities<MapEntityConfig>(this._mapCardConfig.extra_entities, false)
+        : processConfigEntities<MapEntityConfig>([this._deviceTrackerEntity], false);
       this._useSingleMapCard = this._mapCardConfig.single_map_card ?? false;
       if (!this._mapCardConfig.maptiler_api_key && this._mapCardConfig.single_map_card) {
         this._useSingleMapCard = false;
@@ -58,6 +69,36 @@ export class PanelMapEditor extends LitElement {
           },
         });
       }
+    }
+
+    if (_changedProperties.has('_yamlMode') && !this._yamlMode) {
+      const oldYamlMode = _changedProperties.get('_yamlMode') as boolean;
+      if (oldYamlMode === true && this._yamlMode === false) {
+        if (this._tmpYamlConfig) {
+          const mapConfig = this._convertToBaseMapConfig(this._tmpYamlConfig);
+          const miniMapConfig = { ...(this._config.mini_map || {}) };
+          this._config = {
+            ...this._config,
+            map_popup_config: {
+              ...miniMapConfig,
+              ...mapConfig,
+            },
+          };
+          fireEvent(this, 'config-changed', {
+            config: this._config,
+          });
+          this._tmpYamlConfig = undefined;
+        }
+      }
+    }
+
+    if (
+      (_changedProperties.has('_useSingleMapCard') && this._useSingleMapCard) ||
+      _changedProperties.has('_yamlMode')
+    ) {
+      setTimeout(() => {
+        this._hideSelectors();
+      }, 0);
     }
   }
 
@@ -87,15 +128,24 @@ export class PanelMapEditor extends LitElement {
       </ha-form>
     `;
 
-    const content = this._yamlEditor
+    const content = this._yamlMode
       ? this._renderYamlEditor()
       : html` <div class="tip-content">
           ${baseMapSection} ${useSingleMap}
           ${!this._useSingleMapCard ? this._renderDefaultMapConfig() : this._renderSingleMapConfig()}
-          <ha-button slot="actions" @click=${() => (this._yamlEditor = true)}> Edit YAML </ha-button>
         </div>`;
 
-    return content;
+    return html`
+      <div class="card-config">
+        ${content}
+        <div style="justify-content: flex-end; display: flex; margin-top: 1rem;">
+          <ha-button
+            @click=${() => (this._yamlMode = !this._yamlMode)}
+            .label=${this._yamlMode ? 'Show UI editor' : 'Show Code editor'}
+          ></ha-button>
+        </div>
+      </div>
+    `;
   }
 
   private _renderDefaultMapConfig() {
@@ -111,17 +161,17 @@ export class PanelMapEditor extends LitElement {
     `;
   }
 
-  private _renderMapCardConfig() {
-    // const mapConfig = this._convertToExtraMapConfig(this._mapCardConfig!, this._mapEntitiesConfig);
+  private _hideSelectors() {
+    const formRoot = this._mapCardEditor?.shadowRoot
+      ?.querySelector('extra-map-editor')
+      ?.shadowRoot?.querySelector('ha-form')?.shadowRoot;
 
-    return html`
-      <hui-card-element-editor
-        .hass=${this.hass}
-        .value=${this._extraMapCardConfig}
-        .lovelace=${this.editor.lovelace}
-        @config-changed=${this._mapCardConfigChanged}
-      ></hui-card-element-editor>
-    `;
+    if (formRoot) {
+      const selectors = formRoot.querySelectorAll('.root > ha-selector') as NodeListOf<HTMLElement>;
+      selectors.forEach((el: HTMLElement) => {
+        el.style.display = 'none'; // hide title and apikey formns
+      });
+    }
   }
 
   private _mapCardConfigChanged(ev: CustomEvent) {
@@ -146,7 +196,15 @@ export class PanelMapEditor extends LitElement {
 
   private _convertToBaseMapConfig(config: ExtraMapCardConfig): Partial<MiniMapConfig> {
     const map_styles = config.custom_styles;
-    const extra_entities = config.entities;
+    let extra_entities: MapEntityConfig[] = [];
+    const entities = config.entities ? processConfigEntities<MapEntityConfig>(config.entities) : [];
+    if (entities.length === 0) {
+      extra_entities = [this._deviceTrackerEntity];
+    } else if (entities.length > 0 && !entities.find((e) => e.entity === this._deviceTrackerEntity.entity)) {
+      extra_entities = [this._deviceTrackerEntity, ...entities];
+    } else {
+      extra_entities = entities;
+    }
     const {
       fit_zones,
       default_zoom,
@@ -172,24 +230,6 @@ export class PanelMapEditor extends LitElement {
   }
 
   private _renderSingleMapConfig() {
-    const haForms = html`
-      <ha-form
-        .hass=${this.hass}
-        .data=${this._mapConfig}
-        .schema=${singleMapConfingSchema(this.hass.localize)}
-        .computeLabel=${this._computeLabel}
-        @value-changed=${this._valueChanged}
-      >
-      </ha-form>
-      <hui-entity-editor
-        .hass=${this.hass}
-        .label=${'Extra Entities to show on map'}
-        .entities=${this._mapEntitiesConfig}
-        .entityFilter=${hasLocation}
-        @entities-changed=${this._entitiesValueChanged}
-      ></hui-entity-editor>
-    `;
-
     return html`
       ${Create.HaAlert({
         message: ALERT_INFO.MAP_SINGLE_CARD,
@@ -201,7 +241,12 @@ export class PanelMapEditor extends LitElement {
           ],
         },
       })}
-      ${this._renderMapCardConfig()}
+      <hui-card-element-editor
+        .hass=${this.hass}
+        .value=${this._extraMapCardConfig}
+        .lovelace=${this.editor.lovelace}
+        @config-changed=${this._mapCardConfigChanged}
+      ></hui-card-element-editor>
     `;
   }
 
@@ -218,39 +263,38 @@ export class PanelMapEditor extends LitElement {
     });
   }
 
-  private _entitiesValueChanged(ev: CustomEvent): void {
-    ev.stopPropagation();
-    if (ev.detail && ev.detail.entities) {
-      this._config = {
-        ...this._config,
-        mini_map: {
-          ...this._config.mini_map,
-          extra_entities: ev.detail.entities,
-        },
-      };
-    }
-    this._mapEntitiesConfig = processConfigEntities<MapEntityConfig>(ev.detail.entities);
-
-    fireEvent(this, 'config-changed', {
-      config: this._config,
-    });
-    console.log('Entities Config:', this._mapEntitiesConfig);
-  }
-
   private _renderYamlEditor(): TemplateResult {
     return html`
       <div id="yaml-editor">
-        <vsc-sub-panel-yaml
-          .hass=${this.hass}
-          .config=${this._config}
-          .configDefault=${this._config.mini_map}
-          .extraAction=${true}
-          @yaml-config-changed=${this._yamlChanged}
-          @close-editor=${() => (this._yamlEditor = false)}
-        >
-        </vsc-sub-panel-yaml>
+        ${!this._useSingleMapCard
+          ? html` <vsc-sub-panel-yaml
+              .hass=${this.hass}
+              .config=${this._config}
+              .configDefault=${this._config.mini_map}
+              .extraAction=${true}
+              @yaml-config-changed=${this._yamlChanged}
+              @close-editor=${() => (this._yamlMode = false)}
+            >
+            </vsc-sub-panel-yaml>`
+          : html`
+              <ha-yaml-editor
+                .hass=${this.hass}
+                .defaultValue=${this._extraMapCardConfig}
+                .copyClipboard=${true}
+                @value-changed=${this._yamlConfigChanged}
+              ></ha-yaml-editor>
+            `}
       </div>
     `;
+  }
+
+  private _yamlConfigChanged(ev: CustomEvent): void {
+    const { isValid, value } = ev.detail;
+    if (!isValid || !this._config) {
+      return;
+    }
+    this._tmpYamlConfig = value as ExtraMapCardConfig;
+    console.log('yaml config changed:', this._tmpYamlConfig);
   }
 
   private _yamlChanged(ev: CustomEvent): void {
