2e1d081502105df5d6e706e18724e321b7b2506d
Viet Ngoc <ngocjohn@me.com>
2025-10-20 01:17:04 +0200

    ci: update post-release workflow to improve bug report template updates

--- scripts/update-bug-template.js
+++ scripts/update-bug-template.js
@@ -1,57 +1,36 @@
 const fs = require('fs');
 const path = require('path');
-const axios = require('axios');
 const yaml = require('js-yaml');
 
 // Paths
 const bugTemplatePath = path.join(__dirname, '../.github/ISSUE_TEMPLATE/BUG_REPORT.yml');
-
-// GitHub API details
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const GITHUB_REPO = process.env.GITHUB_REPO;
-const RELEASE_API_URL = `https://api.github.com/repos/${GITHUB_REPO}/releases`;
-
-// Fetch the latest release tags from GitHub
-const getReleasesTags = async () => {
-  try {
-    const headers = {};
-
-    if (GITHUB_TOKEN) {
-      headers['Authorization'] = `Bearer ${GITHUB_TOKEN}`;
-    }
-
-    const response = await axios.get(RELEASE_API_URL, {
-      headers,
-    });
-
-    let tags = response.data.filter((release) => !release.draft).map((release) => release.tag_name);
-
-    // only last 10 tags
-    tags = tags.slice(0, 10);
-    console.log('Fetched release tags:', tags);
-
-    return tags;
-  } catch (error) {
-    console.error('Error fetching release tags:', error.message);
-    return null;
-  }
-};
 
 // Check if the current tags are different from the new tags
 const isDifferent = (currentTags, newTags) => {
   return newTags.some((tag) => !currentTags.includes(tag));
 };
 
-// Update the bug template with the fetched release tags
-const updateBugTemplate = async () => {
-  const tags = await getReleasesTags();
-  if (!tags) {
-    console.error('No tags fetched, exiting update process.');
-    return; // Exit if no tags were fetched
+module.exports = async ({ github, context }) => {
+  const repo = context.repo;
+  const releasesList = await github.rest.repos.listReleases({
+    owner: repo.owner,
+    repo: repo.repo,
+  });
+  if (releasesList.status !== 200) {
+    throw new Error(`Failed to fetch releases: ${releasesList.status}`);
   }
-  // Read the existing bug template content
-  const bugTemplate = fs.readFileSync(bugTemplatePath, 'utf8');
-  const doc = yaml.load(bugTemplate);
+
+  let tags = releasesList.data.filter((release) => !release.draft).map((release) => release.tag_name);
+
+  tags = tags.slice(0, 10); // Keep only the latest 10 tags
+
+  console.log('Fetched Tags:', tags);
+
+  // update the bug report template if tags have changed
+
+  // Read the current bug report template
+  const bugTemplateContent = fs.readFileSync(bugTemplatePath, 'utf8');
+  const doc = yaml.load(bugTemplateContent);
 
   // Find the version dropdown field
   const versionField = doc.body.find((field) => field.id === 'version');
@@ -60,23 +39,21 @@ const updateBugTemplate = async () => {
     const currentTags = versionField.attributes.options;
 
     if (!isDifferent(currentTags, tags)) {
-      console.log('No changes in tags, bug report template does not need updating.');
+      console.log('No changes in tags. Exiting.');
       return;
     }
+    console.log('Tags have changed, updating template...');
 
-    // Update the versions
+    // Update the options with the new tags
     versionField.attributes.options = tags;
-
     // Convert back to YAML
-    const updatedYaml = yaml.dump(doc, { lineWidth: -1 });
-
-    // Write updated YAML to file
-    fs.writeFileSync(bugTemplatePath, updatedYaml, 'utf8');
-    console.log('Bug report template updated successfully!');
+    const newBugTemplateContent = yaml.dump(doc, { lineWidth: -1 });
+    // Write
+    fs.writeFileSync(bugTemplatePath, newBugTemplateContent, 'utf8');
+    console.log('Bug report template updated successfully.');
+    return true;
   } else {
-    console.error('Version field not found or in unexpected format.');
+    console.log('Version field not found or invalid in the template.');
+    return false;
   }
 };
-
-// Run the update
-updateBugTemplate();
