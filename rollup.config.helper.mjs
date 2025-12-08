b999c706bf15457003cc88f195ad3c7cf22e5b27
Viet Ngoc <ngocjohn@me.com>
2025-12-08 05:21:26 +0100

    chore: update console info styles

--- rollup.config.helper.mjs
+++ rollup.config.helper.mjs
@@ -7,19 +7,60 @@ import postcssPresetEnv from 'postcss-preset-env';
 import postcssLit from 'rollup-plugin-postcss-lit';
 import { description, homepage } from './package.json';
 
+// pad string to target width with spaces to center it
+function pad(str, targetWidth) {
+  let width = str.length;
+  let totalPadding = Math.round(targetWidth - width);
+  if (totalPadding < 0) totalPadding = 0;
+
+  let leftPadding = Math.floor(totalPadding / 2);
+  let rightPadding = Math.ceil(totalPadding / 2);
+  let result = ' '.repeat(leftPadding) + str + ' '.repeat(rightPadding);
+
+  // Final safety net — ensure not under target
+  while (result.length < targetWidth) {
+    result = ' ' + result + ' ';
+  }
+  // Avoid overshooting (rare edge case)
+  while (result.length > targetWidth) {
+    result = result.slice(1, -1);
+  }
+  return result;
+}
+
+function padByWidth(str) {
+  const strWidth = str.length;
+  // add 3 spaces to each side
+  const targetWidth = strWidth + 8;
+  return pad(str, targetWidth);
+}
+
+const styles = {
+  gradient: '#3F5EFB, #FC466B',
+  border: '#3F5EFB',
+  gradientColor: ['#50586C', '#DCE2F0'],
+};
+
 export function logCardInfo(version) {
   const part1 = '🚘 VEHICLE-STATUS-CARD 🚘';
   const part2 = `${version}`;
-  const part1Style =
-    'background-color: #83818f;color: #fff;padding: 2px 4px;border: 1px solid #83818f;border-radius: 2px 0 0 2px;font-family: Roboto,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)';
-  const part2Style =
-    'background-color: transparent;color: #83818f;padding: 2px 3px;border: 1px solid #83818f; border-radius: 0 2px 2px 0;font-family: Roboto,Verdana,Geneva,sans-serif';
+
+  const { gradientColor } = styles;
+  const backStyle = [gradientColor[0], `${gradientColor[0]} 80%`, `${gradientColor[1]} 100%`];
+
+  const part1Gradient = `background: linear-gradient(90deg, ${backStyle.join()});color: ${gradientColor[1]};`;
+  const part2Gradient = `background: linear-gradient(90deg, ${backStyle.reverse().join()});color: ${gradientColor[0]};`;
+  const sharedStyle = `padding: 2px 0px; border: 0.5px solid; text-shadow: 0 2px 2px rgba(1, 1, 1, 0.2); font-family: Roboto,Verdana,Geneva,sans-serif; border-color: ${gradientColor[1]};`;
+
+  const part1Style = `${part1Gradient}${sharedStyle}border-right: none; border-radius: 6px 0 0 6px;`;
+  const part2Style = `${part2Gradient}${sharedStyle}border-left: none; border-radius: 0 6px 6px 0;`;
+
   const repo = `Github: ${homepage}`;
   const sponsor = 'If you like the card, consider supporting the developer: https://github.com/sponsors/ngocjohn';
 
   return `
     console.groupCollapsed(
-      "%c${part1}%c${part2}",
+      "%c${padByWidth(part1)}%c${padByWidth(part2)}",
       '${part1Style}',
       '${part2Style}',
     );
