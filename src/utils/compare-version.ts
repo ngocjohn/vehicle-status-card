f3d4c72ba321a8551274d504424b5eafad2d1029
Viet Ngoc <ngocjohn@me.com>
2026-03-11 16:29:43 +0100

    fix(image-upload): enhance dialog with wa-dialog compability

--- src/utils/compare-version.ts
+++ src/utils/compare-version.ts
@@ -58,3 +58,13 @@ export function compareVersionNumbers(v1: string, v2: string) {
 
   return 0;
 }
+
+export const atLeastVersion = (version: string, major: number, minor: number, patch?: number): boolean => {
+  const [haMajor, haMinor, haPatch] = version.split('.', 3);
+
+  return (
+    Number(haMajor) > major ||
+    (Number(haMajor) === major && (patch === undefined ? Number(haMinor) >= minor : Number(haMinor) > minor)) ||
+    (patch !== undefined && Number(haMajor) === major && Number(haMinor) === minor && Number(haPatch) >= patch)
+  );
+};
